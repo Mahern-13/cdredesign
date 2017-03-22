9645a459e414ad55f28aba3af9b522f2281219b8
Scott Plunkett <iamretiform@github.com>
2017-03-22 00:54:46 -0500

    redid landing page, simplified layout, added modals, social buttons, etc.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,4 +21,47 @@ $(document).ready(function() {
   $(".trigger").click(function() {
     $(".menu").toggleClass("active"); 
   });
+  $('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').focus();
+  });
+  $(".share-more").click(function(){
+    $(this).parent().addClass("expanded");
+  });
+  $(".share-less").click(function(){
+    $(this).parent().removeClass("expanded");
+  });
+
+  angular.module("app", ["app.directives", "app.filters"]);
+
+  function ShareCtrl($scope) {
+    var url = "http://codepen.io";
+    $scope.shares = {
+      facebook: 1234,
+      twitter: 5678,
+      stumbleupon: 0
+    };
+    $.getJSON("http://graph.facebook.com/" + url, function(data) {
+      $scope.shares.facebook = data.shares;
+    });
+    $.getJSON("http://cdn.api.twitter.com/1/urls/count.json?url=" + url + '&callback=?', function(data) {
+      $scope.shares.twitter = data.count;
+    });
+    /*$.getJSON("http://www.stumbleupon.com/services/1.01/badge.getinfo?url=" + url + '&callback=?', function(data) {
+      $scope.shares.stumbleupon = data.result.views;
+    });*/
+  };
+
+  angular.module("app.directives", []);
+  angular.module("app.filters", [])
+  .filter("shortNumber", function () {
+    return function (value) {
+      if (value >= 1000000) {
+        return Math.round(value/1000000) + "M";
+      }
+      if (value >= 1000) {
+        return Math.round(value/1000) + "K";
+      }
+      return value;
+    };
+  });
 });
