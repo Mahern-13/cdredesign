f089af6a42662f4734883fcf7971dbab433c9086
John Cibula <johnjcibula@gmail.com>
2017-03-22 12:56:48 -0500

    student should be able to provide username and email which will be saved into the database to be used later to receive email containing information

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   root 'pages#index'
+
+  resources :pages
+
+  resources :students do
+    member do
+      get :confirm_email
+    end
+  end
+
   # get :student, to: 'pages#student'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
